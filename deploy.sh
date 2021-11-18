9cebc8316606d6eee6d442727dae59dadd1ee924
sridharvumma <sridhar6358@gmail.com>
2021-11-18 07:32:07 +0000

    fifth line to be common

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@
 echo "Sridhar here"
 I am in stage branch
 Fourth change to be common
+Fifth line to be common
--- test.py
+++ test.py
@@ -2,3 +2,4 @@ iam n pythonfile
 This is the second line
 Third line added for staging server
 Fourth change to be common
+Fifth line to be common
